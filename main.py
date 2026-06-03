ce5ebaf39d93cac827c322af9ae6cede78a90a42
Vipul Shrivastav <vipulmpgi@gmail.com>
2026-06-03 10:46:00 +0530

    Only background and foreground problem is cimming

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ VIDEO_PATH = "videos/conveyor.mp4"
 cap = cv2.VideoCapture(VIDEO_PATH)
 
 count = 0
-counted = set()
+counted = False
+frame_number = 0
 
+# Put line near rollers
 line_y = 300
 
 while True:
@@ -19,8 +21,32 @@ while True:
     if not ret:
         break
 
+    frame_number += 1
+
     contours, mask = detect_objects(frame)
 
+    # Allow background subtractor to learn
+    if frame_number < 50:
+
+        cv2.putText(
+            frame,
+            "Learning Background...",
+            (20, 50),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 255, 255),
+            2
+        )
+
+        cv2.imshow("Frame", frame)
+        cv2.imshow("Mask", mask)
+
+        if cv2.waitKey(20) & 0xFF == 27:
+            break
+
+        continue
+
+    # Blue counting line
     cv2.line(
         frame,
         (0, line_y),
@@ -29,17 +55,23 @@ while True:
         2
     )
 
+    object_detected = False
+
     for contour in contours:
 
         area = cv2.contourArea(contour)
 
+        # Ignore tiny noise
         if area < 2000:
             continue
+
+        object_detected = True
 
         x, y, w, h = cv2.boundingRect(contour)
 
         cx, cy = get_center(x, y, w, h)
 
+        # Green box
         cv2.rectangle(
             frame,
             (x, y),
@@ -48,6 +80,7 @@ while True:
             2
         )
 
+        # Red center point
         cv2.circle(
             frame,
             (cx, cy),
@@ -56,11 +89,29 @@ while True:
             -1
         )
 
-        object_id = f"{cx}_{cy}"
+        # Show CY on frame
+        cv2.putText(
+            frame,
+            f"CY:{cy}",
+            (cx + 10, cy),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.6,
+            (255, 255, 255),
+            2
+        )
 
-        # if cy > line_y and object_id not in counted:
-        #     count += 1
-        #     counted.add(object_id)
+        print("CY =", cy)
+
+        # Counting logic
+        if cy > line_y and not counted:
+            count += 1
+            counted = True
+
+            print("COUNT =", count)
+
+    # Reset when object disappears
+    if not object_detected:
+        counted = False
 
     cv2.putText(
         frame,
