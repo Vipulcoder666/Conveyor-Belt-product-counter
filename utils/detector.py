ecc57213c8e98f47bb1bbf55855349d182fc73dc
Vipul Shrivastav <vipulmpgi@gmail.com>
2026-06-04 09:58:07 +0530

    Detector.py updated, and the background bacome totally black

--- utils/detector.py
+++ utils/detector.py
@@ -7,9 +7,37 @@ bg_subtractor = cv2.createBackgroundSubtractorMOG2(
 )
 
 def detect_objects(frame):
-    mask = bg_subtractor.apply(frame)
 
-    _, mask = cv2.threshold(mask, 200, 255, cv2.THRESH_BINARY)
+    # ROI (adjust if needed)
+    roi = frame[100:650, 0:1280]
+
+    mask = bg_subtractor.apply(roi)
+
+    _, mask = cv2.threshold(
+        mask,
+        200,
+        255,
+        cv2.THRESH_BINARY
+    )
+
+    kernel = cv2.getStructuringElement(
+        cv2.MORPH_RECT,
+        (5, 5)
+    )
+
+    # Remove small noise
+    mask = cv2.morphologyEx(
+        mask,
+        cv2.MORPH_OPEN,
+        kernel
+    )
+
+    # Fill gaps inside object
+    mask = cv2.morphologyEx(
+        mask,
+        cv2.MORPH_CLOSE,
+        kernel
+    )
 
     contours, _ = cv2.findContours(
         mask,
